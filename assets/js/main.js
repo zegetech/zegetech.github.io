b6bb44e0a80c05c770f5a249f1e9f4e56c01973c
gathuku <mosesgathuku95@gmail.com>
2019-10-15 18:17:56 +0300

    add dropdown slide up and down

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,6 +59,15 @@ $(document).ready(function() {
 
 });
 
+$('.dropdown').on('show.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+});
+
+// Add slideUp animation to Bootstrap dropdown when collapsing.
+$('.dropdown').on('hide.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+});
+
 document.getElementById('theDropdown').addEventListener('click',function(){
   event.stopPropagation();
 
